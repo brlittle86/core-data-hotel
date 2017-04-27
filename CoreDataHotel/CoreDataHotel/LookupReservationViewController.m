f5acef32d65061986756ec48f76f57f0bac80c7a
Brandon Little <brlittle86@gmail.com>
2017-04-26 18:37:48 -0700

    attempted to make searchBar work, no workie

--- CoreDataHotel/CoreDataHotel/LookupReservationViewController.m
+++ CoreDataHotel/CoreDataHotel/LookupReservationViewController.m
@@ -22,37 +22,81 @@
 #import "Room+CoreDataClass.h"
 #import "Room+CoreDataProperties.h"
 
-@interface LookupReservationViewController () <UITableViewDataSource>
+@interface LookupReservationViewController () <UITableViewDataSource, UISearchBarDelegate>
 
 @property(strong, nonatomic)NSArray *allReservations;
 @property(strong, nonatomic)UITableView *tableView;
+@property(strong, nonatomic)UISearchBar *searchBar;
+@property(strong, nonatomic)NSArray *searchResult;
 
 @end
 
 @implementation LookupReservationViewController
+
+{
+    NSMutableArray *tableDataArray;
+    BOOL searchEnabled;
+}
 
 - (void)loadView{
     [super loadView];
     
     self.view.backgroundColor = [UIColor whiteColor];
     
-    self.tableView = [[UITableView alloc]init];
-    
-    self.tableView.dataSource = self;
-    
-    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
-    
-    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
-    
-    [self.view addSubview:self.tableView];
-    
-    [AutoLayout fullScreenConstraintsWithVFLForView:self.tableView];
+    [self setupViewLayout];
     
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    tableDataArray = [[NSMutableArray alloc]initWithObjects:self.allReservations, nil];
+    self.searchResult = [NSMutableArray arrayWithCapacity:[tableDataArray count]];
+}
+
+- (void)setupViewLayout{
+    self.searchBar = [[UISearchBar alloc]init];
+    self.searchBar.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.searchBar.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.searchBar.keyboardType = UIKeyboardTypeAlphabet;
+    
+    self.tableView = [[UITableView alloc]init];
+    
+    self.searchBar.delegate = self;
+    self.tableView.dataSource = self;
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    
+    self.searchBar.translatesAutoresizingMaskIntoConstraints = NO;
+    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.view addSubview:self.searchBar];
+    [self.view addSubview:self.tableView];
+    
+    float navBarHeight = CGRectGetHeight(self.navigationController.navigationBar.frame);
+    
+    CGFloat statusBarHeight = 20.0;
+    CGFloat topMargin = navBarHeight + statusBarHeight;
+    CGFloat windowHeight = self.view.frame.size.height;
+    CGFloat frameHeight = (windowHeight - topMargin - statusBarHeight);
+    
+    NSDictionary *viewDictionary = @{@"searchBar": self.searchBar,
+                                     @"tableView": self.tableView};
+    
+    NSDictionary *metricsDictionary = @{@"topMargin": [NSNumber numberWithFloat:topMargin], @"frameHeight": [NSNumber numberWithFloat:frameHeight]};
+    
+    NSString *visualFormatString = @"V:|-topMargin-[searchBar(==topMargin)][tableView(==frameHeight)]|";
+    
+    [AutoLayout leadingConstraintFrom:self.searchBar toView:self.view];
+    [AutoLayout trailingConstraintFrom:self.searchBar toView:self.view];
+    [AutoLayout leadingConstraintFrom:self.tableView toView:self.view];
+    [AutoLayout trailingConstraintFrom:self.tableView toView:self.view];
+    
+    [AutoLayout constraintsWithVFLForViewDictionary:viewDictionary forMetricsDictionary:metricsDictionary withOptions:0 withVisualFormat:visualFormatString];
+    
+    [self.searchBar setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.tableView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
 }
 
 - (NSArray *)allReservations{
@@ -72,8 +116,8 @@
         }
         
         _allReservations = reservations;
+        NSLog(@"%@", self.allReservations[0][@"guest"]);
     }
-    
     return _allReservations;
 }
 
@@ -82,8 +126,12 @@
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
     }
-    
-    Reservation *reservation = self.allReservations[indexPath.row];
+    Reservation *reservation;
+    if (searchEnabled) {
+        reservation = self.searchResult[indexPath.row];
+    } else {
+        reservation = self.allReservations[indexPath.row];
+    }
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"MM-dd-yyyy"];
     
@@ -92,13 +140,55 @@
     
     NSString *formattedEndDateString = [dateFormatter stringFromDate:reservation.endDate];
     
-    cell.textLabel.text = [NSString stringWithFormat:@"%@ %@: %@ in room: %i from %@ to %@", reservation.guest.firstName, reservation.guest.lastName, reservation.room.hotel.name, reservation.room.number, formattedStartDateString, formattedEndDateString];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ %@: %@ in Room: %i, From: %@ to %@", reservation.guest.firstName, reservation.guest.lastName, reservation.room.hotel.name, reservation.room.number, formattedStartDateString, formattedEndDateString];
+    cell.textLabel.numberOfLines = 0;
     
     return cell;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return [self.allReservations count];
+    
+    if (searchEnabled) {
+        return [self.searchResult count];
+    } else {
+        return [self.allReservations count];
+    }
+}
+
+- (void)filterContentForSearchText:(NSString*)searchText
+{
+    NSPredicate *resultPredicate = [NSPredicate predicateWithFormat:@"%K CONTAINS %@", "guest.firstName", searchText];
+    
+    _searchResult = [tableDataArray filteredArrayUsingPredicate:resultPredicate];
+    [self.tableView reloadData];
+}
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
+{
+    if (searchBar.text.length == 0) {
+        searchEnabled = NO;
+        [self.tableView reloadData];
+    }
+    else {
+        searchEnabled = YES;
+        [self filterContentForSearchText:searchBar.text];
+    }
+}
+
+-(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    [searchBar resignFirstResponder];
+    searchEnabled = YES;
+    [self filterContentForSearchText:searchBar.text];
+}
+
+-(void)searchBarCancelButtonClicked:(UISearchBar *)searchBar
+{
+    [searchBar resignFirstResponder];
+    [searchBar setText:@""];
+    searchEnabled = NO;
+    [self.tableView reloadData];
+    
 }
 
 @end
